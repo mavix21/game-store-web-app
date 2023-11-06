9fa76657dd567a9df342eb4df2892c9189e098ba
mavix21 <marcevizcarra2109@hotmail.com>
2023-11-05 21:05:30 -0500

    Feat: add new page

---
+++ src/pages/game.js
@@ -0,0 +1,13 @@
+import '../style.scss';
+import '../components/layout/Cover/Cover.js';
+import '../components/layout/Center/Center.js';
+import '../components/layout/Stack/Stack.js';
+import '../components/layout/Box/Box.js';
+import '../components/layout/Cluster/Cluster.js';
+import '../components/layout/Frame/Frame.js';
+import '../components/layout/Grid/Grid.js';
+import '../components/branding/Carousel/Carousel.js';
+import '../services/Tabs.js';
+
+window.addEventListener('DOMContentLoaded', async () => {
+});
