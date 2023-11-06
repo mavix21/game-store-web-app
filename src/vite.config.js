72476d878a7c0f70ed89db03452eabc5cd0e68da
mavix21 <marcevizcarra2109@hotmail.com>
2023-11-05 21:03:46 -0500

    Feat: configure multi-page build

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,4 +1,19 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
+const root = resolve('src');
+const outDir = resolve('dist');
+
 export default defineConfig({
+  root,
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        game: resolve(root, 'pages', 'index.html')
+      }
+    }
+  }
 });
